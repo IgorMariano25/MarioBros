7c74290198ce136bb07226be4de36a947b027a81
Igor Mariano <igor-mariano@outlook.com>
2023-12-05 18:25:09 -0300

    Adicionando novos componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundVideoComponent } from './Componentes/BackgroundVideo/background.video.component';
+import { ButtonComponent } from './Componentes/Button/button.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { FooterComponent } from './Componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundVideoComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

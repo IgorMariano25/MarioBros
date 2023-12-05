353d63ffdfa2c62a2ed4ab0b9f0739637aa23f95
Igor Mariano <igor-mariano@outlook.com>
2023-12-05 18:25:58 -0300

    Criando TS para o componente background video

---
+++ frontend/src/app/Componentes/BackgroundVideo/background.video.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'backgroundVideo',
+  templateUrl: './background.video.component.html',
+  styleUrl: './background.video.component.css'
+})
+export class BackgroundVideoComponent {
+
+}

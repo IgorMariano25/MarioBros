4ca981c5ed65131b3f40ffedb9311a7a180a8310
Igor Mariano <igor-mariano@outlook.com>
2023-12-06 16:22:37 -0300

    Criando constante para responsividade mobile

--- frontend/src/app/Componentes/header/header.component.ts
+++ frontend/src/app/Componentes/header/header.component.ts
@@ -8,3 +8,12 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
 }
+
+const menuMobile = document.querySelector(".container__header__mobile");
+const openMenuMobile = document.querySelector("#open__menu");
+const closeMenuMobile = document.querySelector("#close__menu");
+
+const desktopLinks = document.querySelectorAll('.container__header a[href^="#"]')
+const mobileLinks = document.querySelectorAll('.container__header a[href^="#"]')
+
+const allMyLinks = [...desktopLinks, ...mobileLinks];

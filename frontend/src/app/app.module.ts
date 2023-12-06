0b985d9ecf78ba3bc6a4cef756840671c6511289
Igor Mariano <igor-mariano@outlook.com>
2023-12-06 14:18:25 -0300

    Removendo componentes da rota

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,19 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackgroundVideoComponent } from './Componentes/BackgroundVideo/background.video.component';
-import { ButtonComponent } from './Componentes/Button/button.component';
 import { HeaderComponent } from './Componentes/header/header.component';
 import { FooterComponent } from './Componentes/footer/footer.component';
-import { MainComponent } from './Componentes/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackgroundVideoComponent,
-    ButtonComponent,
     HeaderComponent,
-    FooterComponent,
-    MainComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,

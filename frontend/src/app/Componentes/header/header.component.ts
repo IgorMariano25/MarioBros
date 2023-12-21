5b1dca8cd721088abc032db3ac03d55655fc4db0
Igor Mariano <igor-mariano@outlook.com>
2023-12-21 14:17:48 -0300

    Enviando alterações

--- frontend/src/app/Componentes/header/header.component.ts
+++ frontend/src/app/Componentes/header/header.component.ts
@@ -9,11 +9,11 @@ export class HeaderComponent {
 
 }
 
-const menuMobile = document.querySelector(".container__header__mobile");
-const openMenuMobile = document.querySelector("#open__menu");
-const closeMenuMobile = document.querySelector("#close__menu");
+// const menuMobile = document.querySelector(".container__header__mobile");
+// const openMenuMobile = document.querySelector("#open__menu");
+// const closeMenuMobile = document.querySelector("#close__menu");
 
-const desktopLinks = document.querySelectorAll('.container__header a[href^="#"]')
-const mobileLinks = document.querySelectorAll('.container__header a[href^="#"]')
+// const desktopLinks = document.querySelectorAll('.container__header a[href^="#"]')
+// const mobileLinks = document.querySelectorAll('.container__header a[href^="#"]')
 
-const allMyLinks = [...desktopLinks, ...mobileLinks];
+// const allMyLinks = [...desktopLinks, ...mobileLinks];
